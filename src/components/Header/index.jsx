2ec8d1611a3ab205a555615013aa5844ff927634
nowshad-islam-dev <mdnowshadislam.2108033.ruet.mte@gmail.com>
2025-10-06 18:02:21 +0600

    add header, layout and input custom component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,45 @@
+import { NavLink, Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
+
+export default function Header() {
+  return (
+    <Navbar collapseOnSelect expand="lg" variant="dark" bg="dark">
+      <Container>
+        <Link className="navbar-brand" to="/">
+          ClickHaat
+        </Link>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            {/* <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown> */}
+          </Nav>
+          <Nav>
+            <li className="nav-item">
+              <NavLink to="/signin" className="nav-link">
+                Signin
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/signup" className="nav-link">
+                Signup
+              </NavLink>
+            </li>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
---
+++ src/components/UI/Input/index.jsx
@@ -0,0 +1,16 @@
+import { Form } from 'react-bootstrap';
+
+export default function Input(props) {
+  return (
+    <Form.Group className="mb-3">
+      <Form.Label>{props.label}</Form.Label>
+      <Form.Control
+        value={props.value}
+        onChange={props.onChange}
+        type={props.type}
+        placeholder={props.placeholder}
+      />
+      <Form.Text className="text-muted">{props.errorMessage}</Form.Text>
+    </Form.Group>
+  );
+}
