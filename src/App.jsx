8862027f6db454cba824dec92661fd7399f51e80
nowshad-islam-dev <mdnowshadislam.2108033.ruet.mte@gmail.com>
2025-10-08 01:20:37 +0600

    add protected route for home page using PrivateRoute component

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,18 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Signin from './pages/Signin';
 import Signup from './pages/Signup';
+import PrivateRoute from './HOC/PrivateRoute';
 function App() {
   return (
     <>
       <div className="App">
         <Router>
           <Routes>
-            <Route path="/" element={<Home />} />
+            {/* Protected Routes */}
+            <Route element={<PrivateRoute />}>
+              <Route path="/" element={<Home />} />
+            </Route>
+
             <Route path="/signin" element={<Signin />} />
             <Route path="/signup" element={<Signup />} />
           </Routes>
