283c203113481f313645eb855e6cd30897da4371
nowshad-islam-dev <mdnowshadislam.2108033.ruet.mte@gmail.com>
2025-10-08 01:19:55 +0600

    implement login functionality with async thunk and axios instance

--- src/app/store/authSlice/index.js
+++ src/app/store/authSlice/index.js
@@ -1,18 +1,63 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { apiBaseUrl } from '../../../config/index';
 
-const initialState = { email: '', password: '' };
+const authAxiosInstance = axios.create({
+  baseURL: apiBaseUrl,
+});
+
+// loading --->>> 'idle' | 'pending' | 'succeeded' | 'failed'
+
+const initialState = {
+  token: localStorage.getItem('click_haat_token') ?? null,
+  user: {},
+  loading: 'idle',
+};
+
+export const loginUserWithCredentials = createAsyncThunk(
+  'auth/loginUser',
+  async (credentials, _thunkApi) => {
+    const res = await authAxiosInstance.post('/auth/admin/signin', credentials);
+    return res.data;
+  }
+);
 
 const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
-    login: (state, action) => {
-      const { email, password } = action.payload;
-      state.email = email;
-      state.password = password;
+    logout: (state) => {
+      state.loading = 'idle';
+      state.token = null;
+      state.user = {};
+      localStorage.removeItem('click_haat_token');
     },
+  },
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(loginUserWithCredentials.fulfilled, (state, action) => {
+        const { token, user } = action.payload;
+        localStorage.setItem('click_haat_token', token);
+        // Either mutate existing state or return new state
+        // to replace old state
+        return {
+          ...state,
+          token,
+          user,
+          loading: 'succeeded',
+        };
+      })
+      .addCase(loginUserWithCredentials.pending, (state) => {
+        state.loading = 'pending';
+      })
+      .addCase(loginUserWithCredentials.rejected, (state) => {
+        state.loading = 'failed';
+        state.token = null;
+        localStorage.removeItem('click_haat_token');
+      });
   },
 });
 
-export const { login } = authSlice.actions;
+export const { logout } = authSlice.actions;
 export default authSlice.reducer;
