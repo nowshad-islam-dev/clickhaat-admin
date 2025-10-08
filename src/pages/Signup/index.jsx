a176a3e7c575781ff7c5aa3b5535b39c7057447c
nowshad-islam-dev <mdnowshadislam.2108033.ruet.mte@gmail.com>
2025-10-08 15:23:33 +0600

    implement sign up functionality

--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -1,14 +1,39 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Container, Form, Button, Row, Col } from 'react-bootstrap';
+import { authAxiosInstance } from '../../app/store/authSlice';
 import Layout from '../../components/Layout';
 import Input from '../../components/UI/Input';
 
 export default function Signup() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function signupUser(e) {
+    setLoading(true);
+    e.preventDefault();
+    const userInfo = { firstName, lastName, email, password };
+    const res = await authAxiosInstance.post('/signup', userInfo);
+    setLoading(false);
+    if (res.status == '200') {
+      navigate('/signin');
+    }
+  }
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <Layout>
       <Container>
         <Row style={{ marginTop: '60px' }}>
           <Col md={{ span: 6, offset: 3 }}>
-            <Form>
+            <Form onSubmit={signupUser}>
               <h3 style={{ marginBottom: '20px', color: '#555' }}>
                 Sign up as Admin
               </h3>
@@ -18,8 +43,10 @@ export default function Signup() {
                     type="text"
                     label="First Name"
                     placeholder="First Name"
-                    value=""
-                    onChange={() => {}}
+                    value={firstName}
+                    onChange={(e) => {
+                      setFirstName(e.target.value);
+                    }}
                   />
                 </Col>
 
@@ -28,8 +55,10 @@ export default function Signup() {
                     type="text"
                     label="Last Name"
                     placeholder="Last Name"
-                    value=""
-                    onChange={() => {}}
+                    value={lastName}
+                    onChange={(e) => {
+                      setLastName(e.target.value);
+                    }}
                   />
                 </Col>
               </Row>
@@ -38,16 +67,20 @@ export default function Signup() {
                 type="email"
                 label="Email"
                 placeholder="Email"
-                value=""
-                onChange={() => {}}
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
               />
 
               <Input
                 type="password"
                 label="Password"
                 placeholder="Password"
-                value=""
-                onChange={() => {}}
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
               />
               <Button variant="primary" type="submit">
                 Signup
