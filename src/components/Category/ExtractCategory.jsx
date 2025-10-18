3c356055b462c897045d5830c9fa0a08e75936b1
nowshad-islam-dev <mdnowshadislam.2108033.ruet.mte@gmail.com>
2025-10-18 15:54:18 +0600

    add separate component for extracting category options

---
+++ src/components/Category/ExtractCategory.jsx
@@ -0,0 +1,42 @@
+import { useMemo, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getCategories } from '@/app/store/categorySlice';
+
+export const extractCategories = (category, result = []) => {
+  for (let cat of category) {
+    result.push({ id: cat.id, name: cat.name });
+    if (cat.children?.length > 0) {
+      extractCategories(cat.children, result);
+    }
+  }
+  return result;
+};
+
+export default function CategoryOptions() {
+  const dispatch = useDispatch();
+  const { loading, category } = useSelector((state) => state.category);
+
+  useEffect(() => {
+    if (loading === 'idle') {
+      dispatch(getCategories());
+    }
+  }, [loading, dispatch]);
+
+  const categoryOptions = useMemo(
+    () => extractCategories(category),
+    [category]
+  );
+
+  return (
+    <>
+      <option value='' disabled>
+        Select parent category
+      </option>
+      {categoryOptions.map((cat) => (
+        <option key={cat.id} value={cat.id}>
+          {cat.name}
+        </option>
+      ))}
+    </>
+  );
+}
