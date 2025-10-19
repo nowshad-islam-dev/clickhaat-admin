8d597b7817fba0d0c1068014bacb77aebafa5dbf
nowshad-islam-dev <mdnowshadislam.2108033.ruet.mte@gmail.com>
2025-10-19 16:46:25 +0600

    rename module from index.js to store.js

--- src/app/store/store.js
+++ src/app/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './authSlice';
 import categoryReducer from './categorySlice';
+import productReducer from './productSlice';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     category: categoryReducer,
+    product: productReducer,
   },
 });
 
