90dbef96547f5851fbdf116b545665e9ab50ddcc
nowshad-islam-dev <mdnowshadislam.2108033.ruet.mte@gmail.com>
2025-11-01 00:11:59 +0600

    add category CRUD operations with automatic category list refresh on create, update, and delete and add global matchers

--- src/app/store/categorySlice/index.js
+++ src/app/store/categorySlice/index.js
@@ -10,11 +10,71 @@ const initialState = {
   error: '',
 };
 
+export const createCategory = createAsyncThunk(
+  'category/createCategory',
+  async (form, { rejectWithValue, dispatch }) => {
+    try {
+      const res = await axiosInstance.post('/category/create', form);
+      if (res.status === 201) {
+        await dispatch(getCategories());
+      }
+    } catch (err) {
+      return rejectWithValue(
+        err.response?.data?.error || 'Failed to create category.'
+      );
+    }
+  }
+);
+
 export const getCategories = createAsyncThunk(
   'category/getCategories',
-  async () => {
-    const res = await axiosInstance.get('/category/all');
-    return res.data.categoryList;
+  async (_, { rejectWithValue }) => {
+    try {
+      const res = await axiosInstance.get('/category/all');
+      return res.data.categoryList;
+    } catch (err) {
+      return rejectWithValue(
+        err.response?.data?.error || 'Failed to fetch categories.'
+      );
+    }
+  }
+);
+
+export const updateCategory = createAsyncThunk(
+  'category/updateCategory',
+  async (
+    { categoryId, updatedCategoryData },
+    { rejectWithValue, dispatch }
+  ) => {
+    try {
+      const res = await axiosInstance.put(
+        `/category/update/${categoryId}`,
+        updatedCategoryData
+      );
+      if (res.status === 200) {
+        await dispatch(getCategories());
+      }
+    } catch (err) {
+      return rejectWithValue(
+        err.response?.data?.error || 'Failed to update category.'
+      );
+    }
+  }
+);
+
+export const deleteCategory = createAsyncThunk(
+  'category/deleteCategory',
+  async (categoryId, { rejectWithValue, dispatch }) => {
+    try {
+      const res = await axiosInstance.delete(`/category/delete/${categoryId}`);
+      if (res.status === 200) {
+        await dispatch(getCategories());
+      }
+    } catch (err) {
+      return rejectWithValue(
+        err.response?.data?.error || 'Failed to delete category.'
+      );
+    }
   }
 );
 
@@ -28,19 +88,33 @@ const categorySlice = createSlice({
       .addCase(logout, () => {
         return initialState;
       })
-      .addCase(getCategories.pending, (state) => {
-        state.loading = 'pending';
-      })
       .addCase(getCategories.fulfilled, (state, action) => {
-        state.category = action.payload;
-        state.loading = 'succeeded';
-        state.error = '';
+        state.category = action.payload || [];
       })
-      .addCase(getCategories.rejected, (state) => {
+      .addCase(getCategories.rejected, (state, action) => {
         state.category = [];
-        state.loading = 'failed';
-        state.error = 'Error fetching categories.';
-      });
+      })
+
+      // Global Matchers
+      .addMatcher(
+        (action) => action.type.endsWith('/fulfilled'),
+        (state) => {
+          (state.loading = 'succeeded'), (state.error = '');
+        }
+      )
+      .addMatcher(
+        (action) => action.type.endsWith('/pending'),
+        (state) => {
+          (state.loading = 'pending'), (state.error = '');
+        }
+      )
+      .addMatcher(
+        (action) => action.type.endsWith('/rejected'),
+        (state, action) => {
+          (state.loading = 'failed'),
+            (state.error = action.payload || 'Something went wrong.');
+        }
+      );
   },
 });
 
